f72de7d5e91130e3ffa9ee9ae1b35c9ff157c5c4
TheQuagga <48164051+TheQuagga@users.noreply.github.com>
2020-07-03 17:30:49 -0400

    Add files via upload
    basic functionality including google API

--- test 1.py
+++ test 1.py
@@ -32,28 +32,74 @@ def revert(var):
         var -= 24
     return var
 
-delta = 8 #placeholder for time zone difference input
+#step 1: departure / arrival date and time
+departure_string = "21 June 2018, 3:00" #basic format, liable for change
 
 '''in the form of (year, month, day, hour, minute, second, weekday, day
 of the year, and daylight savings)'''
-departure_string = "21 June 2018, 3:00" #basic format, could add hour separately
 departure = time.strptime(departure_string, "%d %B %Y, %H:%M")
+#facilitates changing to seconds after epoch
+
 arrival_string = "21 June 2018, 9:00"
 arrival = time.strptime(arrival_string, "%d %B %Y, %H:%M")
 redeparture_string = "26 June 2018"
 redeparture = time.strptime(redeparture_string, "%d %B %Y")
 
-'''in the form of (hour, minute)'''
+dep_time = time.mktime(departure) #converts into seconds after epoch
+arr_time = time.mktime(arrival)
+
+#step 2: departure / arrival location
+
+def gather_coords(city):
+    '''Converts a given city into approximate coordinates in a tuple.'''
+    response = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address={0}&key=AIzaSyC1PSvX7NUotfAZWY68hT6kgHrtVxrrg8g".format(city))
+    data = (json.loads(response.content))["results"][0]["geometry"]["location"]
+    return (data["lat"], data["lng"])
+
+depart_city = "Montreal" # input here
+arrive_city = "Beijing" # spaces swapped with +
+
+lat1, long1 = gather_coords(depart_city)
+lat2, long2 = gather_coords(arrive_city)
+
+def gather_timezone(lat, long, time):
+    '''Converts given coordinates into the designated time zone, returning
+    hours since UTC.'''
+    response = requests.get("https://maps.googleapis.com/maps/api/timezone/json?location={0},{1}&timestamp={2}&key=AIzaSyC1PSvX7NUotfAZWY68hT6kgHrtVxrrg8g".format(lat,long,time))
+    data = json.loads(response.content)
+    return (data["rawOffset"]) #contains the hours offset from UTC
+
+depart_tz = gather_timezone(lat1, long1, dep_time)
+arrive_tz = gather_timezone(lat2, long2, arr_time)
+delta = ((arrive_tz - depart_tz)//3600) #difference between dep / arr
+print(delta)
+
+#step 3: sleep schedule
+
 sleep_start = 23.0
 sleep_end = 7.0
 
+'''the below function defines lowest body temperature point (lbt)'''
+if sleep_end < sleep_start: #midnight is passed
+    if sleep_end - sleep_start + 24 < 7:
+        lbt = sleep_end - 2
+    else:
+        lbt = sleep_end - 3
+elif sleep_end - sleep_start < 7:
+    lbt = sleep_end - 2
+else:
+    lbt = sleep_end - 3
+lbt = revert(lbt)
+
+#step 4: shift day
+
 shift_day = "19 June 2018"
 shift = time.strptime(shift_day, "%d %B %Y")
+shift_time = time.mktime(shift)
+if shift_time > dep_time: #in case the input is later than the dest.
+    shift_time = dep_time #assume that they will not shift earlier
 
-r1 = time.mktime(departure) #converts into seconds after epoch
-r2 = time.mktime(shift)
-
-days_before = int((r1-r2) // 86400) #determines days before departure date
+days_before = int((dep_time-shift_time) // 86400) #determines days before dep
 
 change = delta // days_before #determines the necessary shift of sleep
 
@@ -76,28 +122,4 @@ delay/advance in sleep schedule, ignoring all recommended restrictions'''
     sleep_start = revert(sleep_start + change)
     sleep_end = revert(sleep_end + change)
     print(sleep_start, sleep_end)
-
-#converts a given city into approximate coordinates
-city = "Beijing" # input here
-response2 = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address={0}&key=AIzaSyC1PSvX7NUotfAZWY68hT6kgHrtVxrrg8g".format(city))
-y = json.loads(response2.content)
-y2 = y["results"][0]["geometry"]["location"]
-lat, long = y2["lat"], y2["lng"]
-
-#converts coordinates to a designated time zone (use raw offset)
-response = requests.get("https://maps.googleapis.com/maps/api/timezone/json?location={0},{1}&timestamp={2}&key=AIzaSyC1PSvX7NUotfAZWY68hT6kgHrtVxrrg8g".format(lat,long,time.time()))
-x = json.loads(response.content)
-tz1 = x["rawOffset"] // 3600 #contains the hours offset from UTC
-print(tz1)
-
-
-
-''' disregard for now
-class Time:
-    def __init__(self, string): #in the form HH:MM AM/PM
-        self.hour = int(string[0:1])
-        self.minute = int(string[2:3])
-        if string[4:5] == PM:
-            self.hour += 12
-            '''
 
